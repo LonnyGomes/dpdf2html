38f9f0fd2666b4e0918e7f4f5741121659344ba5
Lonny Gomes <code@lonnygomes.com>
2016-09-05 09:26:55 -0400

    Added base structure of unit tests for parse-ps and index

---
+++ test/index-test.js
@@ -0,0 +1,7 @@
+/*jslint node: true */
+/*globals describe, it */
+var expect = require('chai').expect;
+
+describe('dpdf2html', function () {
+    //TODO add tests for index
+});
---
+++ test/parse-ps-test.js
@@ -0,0 +1,12 @@
+/*jslint node: true */
+/*globals describe, it */
+var expect = require('chai').expect;
+var ps = require('../lib/parse-ps');
+
+describe('dpdf2html', function () {
+    describe('parse', function () {
+        it('should work', function () {
+            expect(2).to.equal(2);
+        });
+    });
+});
