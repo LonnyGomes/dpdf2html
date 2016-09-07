288a0a52febfe9303aa686e8950b230c45f6c7b7
Lonny Gomes <code@lonnygomes.com>
2016-09-06 20:48:30 -0400

    Wrapped all parse functions in their own describe
    This better documents/defines the API the parse function returns

--- test/parse-ps-test.js
+++ test/parse-ps-test.js
@@ -20,31 +20,37 @@ describe('parse', function () {
         });
     });
 
-    it('should return dimension of PDF as a rectangle object', function () {
-        return ps.parse(paths.p4_l1).then(function (d) {
-            var dims = d.getDimensions();
-            expect(dims.x).to.equal(0);
-            expect(dims.y).to.equal(0);
-            expect(dims.width).to.equal(612);
-            expect(dims.height).to.equal(792);
+    describe('getDimensions', function (){
+        it('should return dimension of PDF as a rectangle object', function () {
+            return ps.parse(paths.p4_l1).then(function (d) {
+                var dims = d.getDimensions();
+                expect(dims.x).to.equal(0);
+                expect(dims.y).to.equal(0);
+                expect(dims.width).to.equal(612);
+                expect(dims.height).to.equal(792);
+            });
         });
     });
 
-    it('should return page totals', function () {
-        return ps.parse(paths.p4_l1).then(function (d) {
-            expect(d.getPageTotals()).to.equal(4);
+    describe('getPageTotals', function () {
+        it('should return page totals', function () {
+            return ps.parse(paths.p4_l1).then(function (d) {
+                expect(d.getPageTotals()).to.equal(4);
+            });
         });
     });
 
-    it('should return creation date as a Date object', function () {
-        return ps.parse(paths.p4_l1).then(function (d) {
-            var result = d.getCreationDate();
-            expect(result.getFullYear()).to.equal(2016);
-            expect(result.getMonth()).to.equal(8);
-            expect(result.getDate()).to.equal(2);
-            expect(result.getHours()).to.equal(9);
-            expect(result.getMinutes()).to.equal(40);
-            expect(result.getSeconds()).to.equal(38);
+    describe('getCreationDate', function () {
+        it('should return creation date as a Date object', function () {
+            return ps.parse(paths.p4_l1).then(function (d) {
+                var result = d.getCreationDate();
+                expect(result.getFullYear()).to.equal(2016);
+                expect(result.getMonth()).to.equal(8);
+                expect(result.getDate()).to.equal(2);
+                expect(result.getHours()).to.equal(9);
+                expect(result.getMinutes()).to.equal(40);
+                expect(result.getSeconds()).to.equal(38);
+            });
         });
     });
 
